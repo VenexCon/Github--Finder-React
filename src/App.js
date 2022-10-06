decbbceed650199a453d74f0ef8c2d055878018b
VenexCon <venexconsultant@outlook.com>
2022-10-06 21:52:10 +0100

    <update> Updated 404 Page with styling and Links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -8,7 +11,15 @@ function App() {
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
 
-        <main className="container mx-auto px-3 pb-12">Content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+
         <Footer />
       </div>
     </Router>
