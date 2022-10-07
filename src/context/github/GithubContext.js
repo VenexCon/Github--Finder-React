37d7a3a4a16b8f63387eb01bdbd93ea128d03c87
VenexCon <venexconsultant@outlook.com>
2022-10-07 23:50:06 +0100

    <update> updated useState to reducers, now passing state.values

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,5 +1,6 @@
 import { createContext } from "react";
-import { useState } from "react";
+import { useReducer } from "react";
+import GithubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -7,8 +8,13 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  //returns new state, based on the action type
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   const fetchUsers = async () => {
     const response = await fetch(`${GITHUB_URL}/users`, {
@@ -18,12 +24,15 @@ export const GithubProvider = ({ children }) => {
     });
 
     const data = await response.json();
-    setUsers(data);
-    setLoading(false);
+
+    dispatch({ type: "GET_USERS", payload: data });
   };
 
   return (
-    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+    <GithubContext.Provider
+      value={{ users: state.users, loading: state.loading, fetchUsers }}
+      //No longer passing initialState into the values, but the values of the returned reducer state.
+    >
       {children}
     </GithubContext.Provider>
   );
