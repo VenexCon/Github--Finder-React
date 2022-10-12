eda12f0b79c3715a6df375b32c0f5ec3f0b5a453
VenexCon <venexconsultant@outlook.com>
2022-10-12 19:06:03 +0100

    <update> updated spinner.gif

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,4 +1,4 @@
-import userEvent from '@testing-library/user-event'
+/* import userEvent from '@testing-library/user-event' */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
